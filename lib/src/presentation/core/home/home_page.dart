1a81a046cb80c2d46fcf8b4aae398df7b09a245e
Juan Gonzalo Quiroz Cadavid <juanquiroz@seven4n.com>
2021-06-29 15:56:38 -0500

    Changing home folder

--- lib/src/presentation/core/home/home_page.dart
+++ lib/src/presentation/core/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:todolist/src/features/todos/presentation/pages/home/widgets/home_bottom_app_bar.dart';
-import 'package:todolist/src/features/todos/presentation/pages/home/widgets/home_fab.dart';
+import 'package:todolist/src/presentation/core/home/widgets/home_bottom_app_bar.dart';
+import 'package:todolist/src/presentation/core/home/widgets/home_fab.dart';
 import 'package:todolist/src/features/todos/presentation/pages/home/widgets/tasks_list.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,7 +14,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-
-
-
