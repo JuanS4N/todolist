ba9c75e73b82a2cc24d2c7df7401d7dfe3b1bc2d
Herman <hermanmejia@seven4n.com>
2021-06-30 16:42:10 -0500

    Unified tasks and todos only on tasks with all the parameters

--- lib/src/features/todos/domain/entities/task.dart
+++ lib/src/features/todos/domain/entities/task.dart
@@ -1,23 +1,26 @@
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
-import 'todo.dart';
-
-class Task extends Todo {
+class Task {
+  final String id;
+  final String title;
+  final bool completed;
+  final DateTime modified;
   final DateTime? date;
   final String? description;
-  final List<Todo> subtasks; //TODO use references to subtasks
-
+  final List<String> subtasks; //T
   Task({
     String? id,
-    required String title,
-    required DateTime modified,
-    bool completed = false,
+    required this.title,
+    this.completed = false,
+    DateTime? modified,
     this.date,
     this.description,
-    this.subtasks = const <Todo>[],
-  }) : super(title: title, completed: completed, modified: modified, id: id);
+    this.subtasks = const <String>[],
+  })  : this.id = id ?? UniqueKey().toString(),
+        this.modified = modified ?? DateTime.now();
 
   Task copyWith({
     String? title,
@@ -25,29 +28,28 @@ class Task extends Todo {
     DateTime? modified,
     DateTime? date,
     String? description,
-    List<Todo>? subtasks,
+    List<String>? subtasks,
   }) {
     return Task(
-      id: super.id,
-      title: title ?? super.title,
-      completed: completed ?? super.completed,
-      modified: modified ?? super.modified,
+      id: this.id,
+      title: title ?? this.title,
+      completed: completed ?? this.completed,
+      modified: modified ?? this.modified,
       date: date ?? this.date,
       description: description ?? this.description,
       subtasks: subtasks ?? this.subtasks,
     );
   }
 
-  Map<String, dynamic> toMap() {
+  Map<dynamic, dynamic> toMap() {
     return {
-      'id': super.id,
-      'title': super.title,
-      'completed': super.completed,
-      'modified': super.modified.millisecondsSinceEpoch,
+      'id': id,
+      'title': title,
+      'completed': completed,
+      'modified': modified.millisecondsSinceEpoch,
       'date': date?.millisecondsSinceEpoch,
       'description': description,
-      'subtasks':
-          subtasks.isNotEmpty ? subtasks.map((x) => x.toMap()).toList() : null,
+      'subtasks': subtasks,
     };
   }
 
@@ -61,9 +63,7 @@ class Task extends Todo {
           ? DateTime.fromMillisecondsSinceEpoch(map['date'])
           : null,
       description: map['description'],
-      subtasks: map['subtasks'] != null
-          ? List<Todo>.from(map['subtasks']?.map((x) => Todo.fromMap(x)))
-          : [],
+      subtasks: List<String>.from(map['subtasks']),
     );
   }
 
@@ -72,24 +72,128 @@ class Task extends Todo {
   factory Task.fromJson(String source) => Task.fromMap(json.decode(source));
 
   @override
-  String toString() =>
-      'Task(id: ${super.id}, title: ${super.title}, completed: ${super.completed}, modified: ${super.modified}, date: $date, description: $description, subtasks: $subtasks)';
+  String toString() {
+    return 'Task(id: $id, title: $title, completed: $completed, modified: $modified, date: $date, description: $description, subtasks: $subtasks)';
+  }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
     return other is Task &&
-        other.id == super.id &&
-        other.title == super.title &&
-        other.completed == super.completed &&
-        other.modified == super.modified &&
+        other.id == id &&
+        other.title == title &&
+        other.completed == completed &&
+        other.modified == modified &&
         other.date == date &&
         other.description == description &&
         listEquals(other.subtasks, subtasks);
   }
 
   @override
-  int get hashCode =>
-      super.hashCode ^ date.hashCode ^ description.hashCode ^ subtasks.hashCode;
+  int get hashCode {
+    return id.hashCode ^
+        title.hashCode ^
+        completed.hashCode ^
+        modified.hashCode ^
+        date.hashCode ^
+        description.hashCode ^
+        subtasks.hashCode;
+  }
 }
+
+// import 'dart:convert';
+
+// import 'package:flutter/foundation.dart';
+
+// import 'todo.dart';
+
+// class Task extends Todo {
+//   final DateTime? date;
+//   final String? description;
+//   final List<Todo> subtasks; //TODO use references to subtasks
+
+//   Task({
+//     String? id,
+//     required String title,
+//     required DateTime modified,
+//     bool completed = false,
+//     this.date,
+//     this.description,
+//     this.subtasks = const <Todo>[],
+//   }) : super(title: title, completed: completed, modified: modified, id: id);
+
+//   Task copyWith({
+//     String? title,
+//     bool? completed,
+//     DateTime? modified,
+//     DateTime? date,
+//     String? description,
+//     List<Todo>? subtasks,
+//   }) {
+//     return Task(
+//       id: super.id,
+//       title: title ?? super.title,
+//       completed: completed ?? super.completed,
+//       modified: modified ?? super.modified,
+//       date: date ?? this.date,
+//       description: description ?? this.description,
+//       subtasks: subtasks ?? this.subtasks,
+//     );
+//   }
+
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'id': super.id,
+//       'title': super.title,
+//       'completed': super.completed,
+//       'modified': super.modified.millisecondsSinceEpoch,
+//       'date': date?.millisecondsSinceEpoch,
+//       'description': description,
+//       'subtasks':
+//           subtasks.isNotEmpty ? subtasks.map((x) => x.toMap()).toList() : null,
+//     };
+//   }
+
+//   factory Task.fromMap(Map<dynamic, dynamic> map) {
+//     return Task(
+//       id: map['id'],
+//       title: map['title'],
+//       completed: map['completed'],
+//       modified: DateTime.fromMillisecondsSinceEpoch(map['modified']),
+//       date: map['date'] != null
+//           ? DateTime.fromMillisecondsSinceEpoch(map['date'])
+//           : null,
+//       description: map['description'],
+//       subtasks: map['subtasks'] != null
+//           ? List<Todo>.from(map['subtasks']?.map((x) => Todo.fromMap(x)))
+//           : [],
+//     );
+//   }
+
+//   String toJson() => json.encode(toMap());
+
+//   factory Task.fromJson(String source) => Task.fromMap(json.decode(source));
+
+//   @override
+//   String toString() =>
+//       'Task(id: ${super.id}, title: ${super.title}, completed: ${super.completed}, modified: ${super.modified}, date: $date, description: $description, subtasks: $subtasks)';
+
+//   @override
+//   bool operator ==(Object other) {
+//     if (identical(this, other)) return true;
+
+//     return other is Task &&
+//         other.id == super.id &&
+//         other.title == super.title &&
+//         other.completed == super.completed &&
+//         other.modified == super.modified &&
+//         other.date == date &&
+//         other.description == description &&
+//         listEquals(other.subtasks, subtasks);
+//   }
+
+//   @override
+//   int get hashCode =>
+//       super.hashCode ^ date.hashCode ^ description.hashCode ^ subtasks.hashCode;
+// }
